8233d0ec325249b338b22bf5a359a706b5e3a6e7
yxdhii <yadhirasaavedra069@gmail.com>
2026-05-05 03:32:13 -0500

    feat: agregar Spring Security #3

---
+++ src/main/java/com/productos/productos/auth/CustomUserDetail.java
@@ -0,0 +1,5 @@
+package com.productos.productos.auth;
+
+public class CustomUserDetail {
+    
+}
---
+++ src/main/java/com/productos/productos/auth/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.productos.productos.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class SecurityConfig {
+
+     @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
+            )
+            .csrf(csrf -> csrf.disable()); // Desactiva CSRF solo durante desarrollo
+
+        return http.build();
+    }
+    
+}
